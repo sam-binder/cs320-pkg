e3c0cf3a8960993e87a21b87af4ed8d14ad8f637
Sam <sbb7725@rit.edu>
2019-04-13 13:11:55 -0400

    added some helper functions for dealing with check digits in Util

---
+++ src/RegExModel/BadTrackingNumberFormatException.java
@@ -0,0 +1,8 @@
+package RegExModel;
+
+/**
+ * Created by sambi on 4/13/2019.
+ */
+public class BadTrackingNumberFormatException extends Exception {
+    public BadTrackingNumberFormatException(){ super("That tracking number is not correct");}
+}
